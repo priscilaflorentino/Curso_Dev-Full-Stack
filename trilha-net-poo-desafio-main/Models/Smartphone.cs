ca8f8a79d211d44bb3196eae90731d346894dc43
priscilaflorentino <priscilagonsalez@gmail.com>
2024-08-25 11:55:59 -0300

    Update Smartphone.cs
    Tirar os comentários "TODO"

--- trilha-net-poo-desafio-main/Models/Smartphone.cs
+++ trilha-net-poo-desafio-main/Models/Smartphone.cs
@@ -7,8 +7,6 @@ namespace DesafioPOO.Models
         private string IMEI;
         private int Memoria;
         
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
