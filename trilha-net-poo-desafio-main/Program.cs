ab2e15004ab4b660d86db7353f88b06cb9a1ac64
priscilaflorentino <priscilagonsalez@gmail.com>
2024-08-25 11:54:43 -0300

    Alteração de arquivo
    Alteração da classe de acordo com especificação do desafio "Criando um Sistema e Abstraindo um Celular com POO em C#"

--- trilha-net-poo-desafio-main/Program.cs
+++ trilha-net-poo-desafio-main/Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "99999-1000", modelo: "Nokia 1.0", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Nokia(numero: "99999-2000", modelo: "Iphone 10", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
